bccbfaec4f6d72c494f72adc967febb583b660bb
Gregory Casamento <greg.casamento@gmail.com>
2023-08-15 08:38:09 -0400

    Initial Commit

---
+++ NSTextViewXib_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSTextViewXib_test
+//
+//  Created by Gregory John Casamento on 8/15/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
